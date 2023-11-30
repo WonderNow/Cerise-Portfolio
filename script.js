726f8ad0c9f140f50b7fe204f379e25430381cec
Tom Gasche <tomgasche@hotmail.com>
2023-11-30 15:27:47 +0100

    Chargement des img amélioré + lien vers www.tomgasche.com

--- script.js
+++ script.js
@@ -33,3 +33,45 @@ setTimeout(function(){
 window.addEventListener('pagehide', function(event) {
     window.scrollTo(0, 0); // Fait défiler la page en haut
 });
+
+
+//********
+// Lenis Smooth Scroll
+//********
+document.addEventListener('DOMContentLoaded', function() {
+    let lenis;
+    if (typeof Lenis !== 'undefined') {
+      lenis = new Lenis({
+        lerp: 0.1,
+        wheelMultiplier: 1,
+        gestureOrientation: 'vertical',
+        normalizeWheel: false,
+        smoothTouch: false
+      });
+  
+      function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
+  
+      requestAnimationFrame(raf);
+  
+      $("[data-lenis-start]").on("click", function () {
+        lenis.start();
+      });
+  
+      $("[data-lenis-stop]").on("click", function () {
+        lenis.stop();
+      });
+  
+      $("[data-lenis-toggle]").on("click", function () {
+        $(this).toggleClass("stop-scroll");
+        if ($(this).hasClass("stop-scroll")) {
+          lenis.stop();
+        } else {
+          lenis.start();
+        }
+      });
+    }
+});
+  
